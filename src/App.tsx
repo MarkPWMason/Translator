a5056d9236b9319f03bd2ab0c784f49337657896
m012192j <m012192j@student.staffs.ac.uk>
2023-03-06 13:38:15 +0000

    Setup HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Translator, {getStringMappings} from './services/Translator';
-import './App.css';
+import styles from './App.module.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Body from './components/Body';
 
 function App() {
   console.log(Translator("the", getStringMappings()));
-  return <div>
-    
+  return <div className={styles.container}>
+    <Header />
+    <Body />
+    <Footer />
   </div>;
   
 }
